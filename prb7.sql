acdbce3fd4e198ede0a382528e6194112366ed47
harpan <146012578+sahilpawar01@users.noreply.github.com>
2024-11-05 19:42:53 +0530

    Update prb7.sql

--- prb7.sql
+++ prb7.sql
@@ -15,6 +15,9 @@ CREATE TABLE Library_Audit (
     Operation_Date DATE
 );
 
+
+SET SERVEROUTPUT ON;
+
 -- Trigger to track updates on Library table
 CREATE OR REPLACE TRIGGER trg_Library_Update
 BEFORE UPDATE ON Library
@@ -50,3 +53,30 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Delete operation performed on Library table.');
 END;
 /
+
+-- Insert Book 1
+INSERT INTO Library (Book_ID, Title, Author) VALUES
+('B001', 'The Great Gatsby', 'F. Scott Fitzgerald');
+
+-- Insert Book 2
+INSERT INTO Library (Book_ID, Title, Author) VALUES
+('B002', 'To Kill a Mockingbird', 'Harper Lee');
+
+-- Insert Book 3
+INSERT INTO Library (Book_ID, Title, Author) VALUES
+('B003', '1984', 'George Orwell');
+
+-- Insert Book 4
+INSERT INTO Library (Book_ID, Title, Author) VALUES
+('B004', 'Pride and Prejudice', 'Jane Austen');
+
+-- Insert Book 5
+INSERT INTO Library (Book_ID, Title, Author) VALUES
+('B005', 'The Catcher in the Rye', 'J.D. Salinger');
+
+COMMIT;
+
+
+SELECT * FROM Library_Audit;
+
+
